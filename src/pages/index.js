a4c28ac3d07586d65197aa2e2b49f194759ee181
Nickchapman3773 <57951940+Nickchapman3773@users.noreply.github.com>
2024-10-11 12:37:09 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,18 @@ import Layout from "@theme/Layout";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-<div class="container">
-  <div class="video-bg cover">
-    <div class="video-fg">
-      <iframe src="https://www.youtube.com/embed/1-mxkgp4XhA?autoplay=1&mute=1&controls=0&loop=1&playlist=1-mxkgp4XhA" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="video-fg supports-cover">
-      <video src="https://i.imgur.com/2uMciu9.mp4"></video>
-    </div>
-  </div>
-</div>
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <div className={styles.buttons}>
+          <Link className="button button--primary button--lg" to="/docs/intro">
+            Click here to begin
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default function Home() {
